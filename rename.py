e8e583bf7d20be87485cc01beb6c2c474608bb0f
tjc <tangjiacheng23@163.com>
2022-04-10 17:17:28 +0800

    v1

--- rename.py
+++ rename.py
@@ -2,7 +2,7 @@ import os
 list_data=os.listdir('./')
 import re
 re_if_kinh = re.compile(r'\[打包下载\]需解压重命名album目录\[(.*?)\]为\[(.*?)\]')
-#下下来的文件名有时改名信息包含.mp4.mp4
+#下下来的文件名有时改名信ss息包含.mp4.mp4
 match_mp4 = re.compile(r'.*?\.mp4')
 for name in list_data:
     result = re_if_kinh.match(name)
